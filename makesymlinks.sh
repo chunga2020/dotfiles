10c71d7ff4601bd8943bdf9abc545266b77de802
Andrew Chung <andrewtc4@gmail.com>
2019-07-20 16:20:46 -0400

    Add xinputrc
    The touchpad and mouse both previously used reverse scrolling. I don't
    mind it for the mouse, but can't stand it for the touchpad. Add xinputrc
    to enable touchpad natural scrolling, while maintaining mouse reverse
    scrolling. Update the symlink script accordingly.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="bash_aliases bashrc gitconfig inputrc imwheelrc tmux.conf vimrc xprofile"
+files="bash_aliases bashrc gitconfig inputrc imwheelrc tmux.conf vimrc xinputrc xprofile"
 
 ##########
 
