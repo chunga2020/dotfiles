b052306675bd6231dfa81272ad66e2b572e0e27a
Andrew Chung <andrewtc4@gmail.com>
2023-06-02 08:52:52 -0400

    qtile/autostart.sh: Remove X11 programs

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-xrdb ~/.Xresources &
 numlockx &
 /usr/bin/dunst &
 alacritty -e ~/bin/arch-upgrade-reminder &
 ~/bin/get-news -s 5 whdh.com wcvb.com reuters.com &
-synclient HorizTwoFingerScroll=1 VertScrollDelta=-28 HorizScrollDelta=-28 TapButton1=0 TapButton2=0 tapbutton3=0 &
-setxkbmap -option ctrl:nocaps -option shift:both_capslock -option compose:menu &
 pulseaudio -k; pulseaudio -D &
 ~/bin/multihead-control &
 unclutter -b &
