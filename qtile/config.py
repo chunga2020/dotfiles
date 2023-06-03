0de3856497927786080b1402536f061f56ad0cc2
Andrew Chung <andrewtc4@gmail.com>
2023-06-03 10:46:32 -0400

    qtile/config: Disable cursor_warp
    Unless there's a way to make cursor_warp not warp to tooltips, this is a real
    pain

--- qtile/config.py
+++ qtile/config.py
@@ -214,7 +214,7 @@ dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
-cursor_warp = True
+cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
