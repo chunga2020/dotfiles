cf8c2833066ad70c6b0819a867677b6cef16cb68
Andrew Chung <andrewtc4@proton.me>
2023-07-31 13:55:33 -0400

    qtile/autostart: Add Xorg-specific config

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ -z $WAYLAND_DISPLAY ]]; then
+    # Qtile on Xorg
+    xrdb ~/.Xresources
+    numlockx on
+    setxkbmap -option compose:menu -option ctrl:nocaps -option shift:bothcapslock
+fi
+
 /usr/bin/dunst &
 alacritty -e ~/bin/arch-upgrade-reminder &
 ~/bin/get-news -b brave -s 5 whdh.com wcvb.com reuters.com &
