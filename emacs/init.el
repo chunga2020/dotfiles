ff0fedd9df50ecad518f224fbb0d5b5e516f147a
Andrew Chung <andrewtc4@proton.me>
2023-09-07 09:01:55 -0400

    emacs/init.el: Rebind C-<delete>
    `delete-region` allows me to delete text without saving it to the kill ring,
    which can be useful if I know a piece of text won't be used elsewhere

--- emacs/init.el
+++ emacs/init.el
@@ -63,6 +63,9 @@
 
 (global-unset-key (kbd "<menu>"))
 (global-set-key (kbd "C-x M-t") 'transpose-sentences)
+(global-unset-key (kbd "C-<delete>"))   ; Default: kill-word (shared with M-d)
+                                        ; and I use M-d far more often
+(global-set-key (kbd "C-<delete>") 'delete-region)
 
 ;; ################################ Packages ###################################
 
