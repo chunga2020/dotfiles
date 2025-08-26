0d96683353864eb814cd35e3d2d6774c77fe422c
Andrew Chung <andrewtc4@proton.me>
2025-08-26 13:08:09 -0400

    emacs: Move custom-set variables to separate file

---
+++ emacs/custom.el
@@ -0,0 +1,20 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eglot-ignored-server-capabilities
+   '(:documentFormattingProvider :documentRangeFormattingProvider
+                                 :documentOnTypeFormattingProvider))
+ '(eglot-prefer-plaintext t)
+ '(package-selected-packages
+   '(browse-kill-ring company darkman highlight-indent-guides hl-todo magit
+                      magit-todos markdown-mode org-mode telephone-line vterm
+                      winum)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:extend t :background "blue" :foreground "white")))))
--- emacs/init.el
+++ emacs/init.el
@@ -1,26 +1,14 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eglot-ignored-server-capabilities
-   '(:documentFormattingProvider :documentRangeFormattingProvider
-                                 :documentOnTypeFormattingProvider))
- '(package-selected-packages
-   '(browse-kill-ring company darkman highlight-indent-guides hl-todo magit
-                      magit-todos markdown-mode org-mode telephone-line vterm
-                      winum)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(region ((t (:extend t :background "cyan" :foreground "#000000")))))
+(setq custom-file (locate-user-emacs-file "custom.el"))
 
 ;; Add MELPA to known package repos
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+ '(package-selected-packages
+   '(browse-kill-ring company darkman highlight-indent-guides hl-todo magit
+                      magit-todos markdown-mode org-mode telephone-line vterm
+                      winum)))
 
 (setq
  inhibit-startup-screen t
