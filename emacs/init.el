9d76f019b39e3e4330e6f878275473db92087134
Andrew Chung <andrewtc4@proton.me>
2025-08-26 11:43:17 -0400

    emacs/init.el: Specify region face
    Ensures the region is visibly highlighted in a TTY frame

--- emacs/init.el
+++ emacs/init.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(region ((t (:extend t :background "cyan" :foreground "#000000")))))
 
 ;; Add MELPA to known package repos
 (require 'package)
