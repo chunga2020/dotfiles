c68e5c9860543b92ad38467ab6ae58f7357217c5
Andrew Chung <andrewtc4@proton.me>
2025-08-29 20:20:00 -0400

    emacs/keybinds: Add new help keybinds

--- emacs/keybinds.el
+++ emacs/keybinds.el
@@ -3,3 +3,11 @@
 (global-unset-key (kbd "C-<delete>"))   ; Default: kill-word (shared with M-d)
                                         ; and I use M-d far more often
 (global-set-key (kbd "C-<delete>") 'delete-region)
+
+;; Default ‘C-h a’ is nice, but it only shows commands.  Re-binding it to
+;; execute ‘apropos’ might make the result list much longer, but that’s a
+;; worthwhile trade for having it return results of all types
+(global-unset-key (kbd "C-h a"))
+(global-set-key (kbd "C-h a c") 'apropos-command)
+(global-set-key (kbd "C-h a f") 'apropos-function)
+(global-set-key (kbd "C-h a v") 'apropos-variable)
