fb5e7dbb9ed7be89d959054e73ae82d8a8686321
Andrew Chung <andrewtc4@proton.me>
2025-09-01 18:03:04 -0400

    emacs/functions: Add countdown-which-album

--- emacs/functions.el
+++ emacs/functions.el
@@ -21,3 +21,22 @@ If split-lines is supplied, resize the new window using `shrink-window'."
            (darkman-mode t)
            (global-hl-line-mode))
     (error "Not running in a GUI frame.")))
+
+(defun countdown-which-album (albums days-left)
+  "Display which of a band's albums to play.
+
+This function helps you build hype for a concert by having you listen to
+albums in sequence in the days up to the show.  ALBUMS should be a list
+of the titles of the albums in question.  This list should have at least
+one element.  DAYS-LEFT should be a non-negative integer corresponding
+to how many days are left before the show."
+  (interactive "XAlbum list: \nnDays left: ")
+  (when (length< albums 2)
+    (error "ALBUMS should have at least two elements."))
+  (when (<= days-left 0)
+    (error "Should have non-negative number of days left."))
+  (let* ((count (length albums))
+         (album-index (% days-left count))
+         (albums-reverse (reverse albums))
+         (the-album (nth album-index albums-reverse)))
+    (message "Today's album is %s" the-album)))
